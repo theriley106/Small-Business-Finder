eeec409e62419ae3e3d5c9335bac8755391de210
theriley106 <christopherlambert106@gmail.com>
2019-08-15 22:35:37 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sbfinder',
+		scripts=['bin/sbfinder'],
+      version='1.0',
+      description='Using the Yelp Fusion API to Find Local Businesses Without an Online Presense',
+      author='Christopher Lambert',
+      author_email='lambertcr@outlook.com',
+      url='https://github.com/theriley106/SmallBusinessFinder',
+      license='MIT',
+      packages=['sbfinder'],
+      install_requires=['requests']
+     )
