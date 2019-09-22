bab7112b11539b39e8b19066ee58382e78024225
theriley106 <christopherlambert106@gmail.com>
2019-09-21 17:13:22 -0700

    Updated version with urllib

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(name='sbfinder',
 		scripts=['bin/sbfinder'],
-      version='1.2',
+      version='2.1',
       description='Using the Yelp Fusion API to Find Local Businesses Without an Online Presense',
       author='Christopher Lambert',
       author_email='lambertcr@outlook.com',
       url='https://github.com/theriley106/SmallBusinessFinder',
       license='MIT',
       packages=['sbfinder'],
-      install_requires=['requests', 'bs4', 'lxml', 'urllib']
+      install_requires=['requests', 'bs4', 'lxml', 'urllib3']
      )
