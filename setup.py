3839a368fa53f19ea750dc39b5c43d67de7c131f
theriley106 <christopherlambert106@gmail.com>
2019-09-21 17:18:38 -0700

    Changed for backwards compatible

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='sbfinder',
 		scripts=['bin/sbfinder'],
-      version='2.1',
+      version='2.2',
       description='Using the Yelp Fusion API to Find Local Businesses Without an Online Presense',
       author='Christopher Lambert',
       author_email='lambertcr@outlook.com',
