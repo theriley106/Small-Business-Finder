d7f577b3f828d8cf3ee7d15001299b9821edeb63
theriley106 <christopherlambert106@gmail.com>
2019-09-21 17:11:29 -0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='sbfinder',
 		scripts=['bin/sbfinder'],
-      version='1.04',
+      version='1.2',
       description='Using the Yelp Fusion API to Find Local Businesses Without an Online Presense',
       author='Christopher Lambert',
       author_email='lambertcr@outlook.com',
